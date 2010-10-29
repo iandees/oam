b9ca1b06d80c02032e5b2e6582aa57283e4268b6
Schuyler Erle <schuyler@simplegeo.com>
2010-10-29 01:50:56 -0700

    Add test suite.

---
+++ accesstools/oampython/oam/test/client.py
@@ -0,0 +1,8 @@
+from oam.client import Client
+import unittest
+
+class ClientTest(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ accesstools/oampython/oam/test/run.py
@@ -0,0 +1,6 @@
+from client import *
+from image import *
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
