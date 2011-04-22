0535826654724cb910cba4dcf61c4e9b0fd64504
Michal Migurski <mike@stamen.com>
2011-04-22 14:34:19 -0700

    Bumped to 0.2.0 with oam.tiles config option and new Makefile

--- accesstools/oampython/setup.py
+++ accesstools/oampython/setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 
 setup(
     name='OAM',
-    version='0.1',
-    description='OpenAerialMap',
+    version='0.2.0',
+    description='Client library for OpenAerialMap (http://oam.osgeo.org)',
     author='Schuyler Erle',
     author_email='schuyler@nocat.net',
-    url='http://github.com/oam/oam/',
+    url='https://github.com/oam/oam/tree/master/accesstools/oampython',
     packages=['oam'],
     scripts=['scripts/oam-post', 'scripts/oam-fetch', 'scripts/oam2vrt']
 )
